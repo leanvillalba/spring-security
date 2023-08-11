ff50f756671c5444406679d2e4dd59d08d228a41
Leandro Villalba <leandrovillalba.dev>
2023-08-10 22:37:33 -0400

    WebSecurityConfig

---
+++ src/main/java/leanvillalba/github/com/springsecurity/configuration/WebSecurityConfig.java
@@ -0,0 +1,11 @@
+package leanvillalba.github.com.springsecurity.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    /*
+    * WebSecurityConfigurerAdapter Deprecada*/
+}
